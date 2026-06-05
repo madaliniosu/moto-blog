c778ca8519e0c43e9a01c5d7de38ad18d910a5f9
105gm <iosu.gm@gmail.com>
2026-06-05 17:16:30 +0200

    "Placed article date as title and made description optional"

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,10 +4,10 @@ import { glob } from "astro/loaders";
 const trips = defineCollection({
   loader: glob({ pattern: '**/index.md', base: './src/content/trips' }),
   schema: z.object({
-    title: z.string(),
+    // title: z.string().optional(),
     date: z.coerce.date(),
     date_end: z.coerce.date().optional(),
-    description: z.string(),
+    description: z.string().optional(),
     youtube: z.string().optional(),
     photos: z.array(z.string()).optional(),
     distance: z.number().optional(),
